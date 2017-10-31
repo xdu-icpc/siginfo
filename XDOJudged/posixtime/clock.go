2227f775cb976730742220f5c714c389f6dbe3de
Xi Ruoyao <ryxi@stu.xidian.edu.cn>
2017-10-31 14:33:32 +0800

    [minor] Fix typos in copyright statement

--- XDOJudged/posixtime/clock.go
+++ XDOJudged/posixtime/clock.go
@@ -7,7 +7,7 @@
 // (at your option) any later version.
 
 // This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warramty of
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
 
