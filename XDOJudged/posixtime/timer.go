b7b310ef1aa3a29375604b35661d2bcfffe8d305
Xi Ruoyao <ryxi@stu.xidian.edu.cn>
2017-11-06 01:41:21 +0800

    [posixtime] Implement timers
    	* timer.go (ClockID.NewTimer, ClockID.AfterFunc): New methods.
    	(Timer.Stop, Timer.Reset): New methods.
    	(Timer.arm): New internal method.
    	* timer_test.go (TestTimer, TestAfterFunc): New tests.

---
+++ XDOJudged/posixtime/timer.go
@@ -0,0 +1,184 @@
+// Manipulate timers on POSIX clocks.
+// Copyright (C) 2017  Laboratory of ACM/ICPC, Xidian University
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Author: Xi Ruoyao <ryxi@stu.xidian.edu.cn>
+
+// +build linux
+
+package posixtime
+
+// We do NOT use timer_* system calls since they rely on OS signals.
+// Insteadly we emulate them using ClockID.Sleep and provide an interface
+// like time.Timer and time.Ticker.
+
+import (
+	"errors"
+	"time"
+)
+
+// The TimerEvent type represents a timer expiration event.
+type TimerEvent struct {
+	// If not nil, the timer has encountered a problem and stopped.
+	Err error
+	// If Err is nil, it contains the clock time of timer expiration.
+	// Otherwise is nil.
+	Time *time.Time
+	// It contains a caller specified value (like sigev_value).
+	Value interface{}
+}
+
+// The Timer type represents a single event.  When the Timer expires, a
+// TimerEvent will be sent on C, unless the Timer was created by
+// AfterFunc. A Timer must be created with ClockID.NewTimer or
+// ClockID.AfterFunc.
+type Timer struct {
+	C <-chan TimerEvent
+
+	activeCh chan bool
+	clock    ClockID
+	handler  func(TimerEvent)
+	value interface{}
+}
+
+// Stop prevents the Timer from firing.  It returns true if the call stops
+// the timer, false if the timer has already expired or been stopped.  Stop
+// does not close the channel, to prevent a read from the channel succeeding
+// incorrectly.
+//
+// To prevent a timer created with ClockID.NewTimer from firing after a call
+// to Stop, check the return value and drain the channel.  Foe example,
+// assuming the program has not received from t.C already:
+//
+//     if !t.Stop() {
+//         <-t.C
+//     }
+//
+// This cannot be done concurrent to other receives from the Timer's
+// channel.
+//
+// For a timer created with ClockID.AfterFunc(d, f), it t.Stop returns
+// false, then the timer has already expired and the function f has been
+// started in its own goroutine; Stop does not wait for f to complete
+// before returning.  If the caller needs to know whether f is completed,
+// it must coordinate with f explicitly.
+func (t *Timer) Stop() bool {
+	active := <-t.activeCh
+	t.activeCh <- false
+	return active
+}
+
+// This internal function set t with empty activeCh, empty C to expire after
+// duration d.  If there is an error setting t, the timer expires
+// immediately.
+func (t *Timer) arm(d time.Duration) {
+	t.activeCh <- true
+	// Create a goroutine to sleep for duration d and call the handler.
+	go func() {
+		// set up a TimerEvent and call handler at last.
+		var ev TimerEvent
+
+		// Sleep a while.
+		err := t.clock.Sleep(d)
+		if err != nil {
+			ev = TimerEvent{err, nil, t.value}
+		}
+
+		// Check if the timer is still active.
+		active := <-t.activeCh
+		t.activeCh <- false
+
+		// This is active.  We should call the handler.
+		if active {
+			if err == nil {
+				now, err := t.clock.GetTime()
+				if err != nil {
+					ev = TimerEvent{err, nil, t.value};
+				} else {
+					ev = TimerEvent{nil, now, t.value}
+				}
+			}
+			t.handler(ev)
+		}
+	}()
+}
+
+// Reset changes the timer to expire after duration d.  It must be used on
+// an inactive timer with a drained t.C.  If a program has already received
+// a value from t.C, the timer is know to have expired, then t.Reset can be
+// used directly.  If not, the timer must be stopped and-if Stop reports
+// that the timer expired before being stopped-the channel explicitly
+// drained:
+//
+//     if !t.Stop() {
+//         <-t.C
+//     }
+//     t.Reset(d)
+//
+// This should not be done concurrent to other receives from the timer's
+// channel.
+//
+// If t is active, Reset would return an error indicating this issue and
+// do nothing.  However if t is inactive but t.C is not drained, Reset can
+// not know this and there may be severe problems. Do NOT do that.
+func (t *Timer) Reset(d time.Duration) error {
+	active := <-t.activeCh
+
+	// Detect an obvious error and report it.
+	// But a t.C not drained can not be detected.
+	if active {
+		t.activeCh <- true
+		return errors.New("should not Reset an active timer")
+	}
+
+	t.arm(d)
+	return nil
+}
+
+// NewTimer creates a new Timer on clock.  The Timer would send a
+// TimerEvent its channel after at least duration d, or encounter an
+// error.
+func (clock ClockID) NewTimer(d time.Duration, v interface{}) *Timer {
+	ch := make(chan TimerEvent)
+	t := Timer{
+		C:        ch,
+		activeCh: make(chan bool, 1),
+		clock:    clock,
+		handler: func(ev TimerEvent) {
+			ch<- ev
+		},
+		value: v,
+	}
+
+	t.arm(d)
+	return &t
+}
+
+// AfterFunc waits for the duration to elapse and then calls f in its own
+// goroutine, with a TimerEvent as arugment.  It returns a Timer that can
+// be used to cancel the call using its Stop method.  If AfterFunc encounter
+// an error, f is called immediately in its own goroutine.
+func (clock ClockID) AfterFunc(d time.Duration, v interface{},
+	f func(TimerEvent)) *Timer {
+	ch := make(chan TimerEvent)
+	t := Timer{
+		C:        ch,
+		activeCh: make(chan bool, 1),
+		clock:    clock,
+		handler:  f,
+	}
+	t.arm(d)
+	return &t
+}
---
+++ XDOJudged/posixtime/timer_test.go
@@ -0,0 +1,147 @@
+// Unitest of timer.go.
+// Copyright (C) 2017  Laboratory of ACM/ICPC, Xidian University
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Author: Xi Ruoyao <ryxi@stu.xidian.edu.cn>
+
+// +build linux
+
+package posixtime_test
+
+import (
+	"fmt"
+	"syscall"
+	"testing"
+	"time"
+
+	"linux.xidian.edu.cn/git/XDU_ACM_ICPC/XDOJ-next/XDOJudged/posixtime"
+)
+
+func TestTimer(t *testing.T) {
+	t0, err := posixtime.CLOCK_MONOTONIC.GetTime()
+	if err != nil {
+		t.Fatalf("can not get time of CLOCK_MONOTONIC: %v", err)
+	}
+
+	// To simplify the code we combine some tests in this function.
+	// Use logs to distinguish them.
+	t.Log("Testing NewTimer...")
+
+	timer := posixtime.CLOCK_MONOTONIC.NewTimer(time.Second, 19260817)
+
+	ev := <-timer.C
+	if ev.Err != nil {
+		t.Fatal(ev.Err)
+	}
+
+	if ev.Value != 19260817 {
+		t.Fatalf("ev.Value = %v, should be 19260817.", ev.Value)
+	}
+	t1 := ev.Time
+
+	err = checkDuration(t1.Sub(*t0), time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("success.")
+
+	t.Log("Testing Reset...")
+	err = timer.Reset(time.Millisecond * 200)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if ev.Value != 19260817 {
+		t.Fatalf("ev.Value = %v, should be 19260817.", ev.Value)
+	}
+
+	ev = <-timer.C
+	if ev.Err != nil {
+		t.Fatal(ev.Err)
+	}
+	t2 := ev.Time
+
+	err = checkDuration(t2.Sub(*t1), time.Millisecond*200)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("success.")
+
+	t.Log("Testing Stop active timer...")
+	err = timer.Reset(time.Millisecond * 100)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !timer.Stop() {
+		t.Fatalf("Stop returned false on active timer!")
+	}
+	gotimer := time.NewTimer(time.Millisecond * 200)
+	select {
+	case ev := <-timer.C:
+		t.Fatalf("get unexpected value from C: %v", ev)
+	case <-gotimer.C:
+	}
+	t.Log("success.")
+
+	t.Log("Testing Stop inactive timer...")
+	timer.Reset(time.Millisecond * 10)
+	time.Sleep(time.Millisecond * 100)
+	if timer.Stop() {
+		t.Fatalf("Stop returned true on inactive timer!")
+	}
+	gotimer.Reset(time.Second)
+	select {
+	case <-gotimer.C:
+		t.Fatalf("lost value in C!")
+	case <-timer.C:
+	}
+	t.Log("success.")
+}
+
+func TestAfterFunc(t *testing.T) {
+	t0 := time.Now()
+	ch := make(chan error)
+	posixtime.CLOCK_MONOTONIC.AfterFunc(time.Millisecond*200, "frog",
+		func(ev posixtime.TimerEvent) {
+			if ev.Err != nil {
+				ch<- ev.Err
+				return
+			}
+			d := time.Since(t0)
+			err := checkDuration(d, time.Millisecond*200)
+			if err != nil && ev.Value != "frog" {
+				err = fmt.Errorf("ev.Value = %v, should be \"frog\".")
+			}
+			ch<- err
+		})
+	gotimer := time.NewTimer(time.Millisecond * 400)
+	select {
+	case <-gotimer.C:
+		t.Fatalf("the func has not been executed at the expected time!")
+	case err := <-ch:
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+func TestError(t *testing.T) {
+	timer := posixtime.CLOCK_THREAD_CPUTIME_ID.NewTimer(time.Second, nil)
+	ev := <-timer.C
+	if ev.Err != syscall.EINVAL {
+		t.Fatalf("tv.Err = %v, should be syscall.EINVAL.", ev.Err)
+	}
+}
